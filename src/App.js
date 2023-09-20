d453d22f8318ab33f87204ac1e386e122f59d4ed
Raghuraj Yadav <shivam982yadav@gmail.com>
2023-09-20 12:10:46 +0530

    Creating Signup Screen

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import SignupForm from './Components/SignupLogin/SignupForm';
+
+function App() {
+  return (
+    <div className="App">
+      <SignupForm />
+    </div>
+  );
+}
+
+export default App;
